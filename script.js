eb464b781022cb79942b6ddbd3d7f9ae342affdb
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-03-14 13:04:50 +0200

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateSyntax() {
   let result = ""; // Placeholder for validation result
 
   // Check if input starts with 'pet_' and followed by alphanumeric characters
-  if (/^pet_[a-zA-Z0-9]+$/.test(input)) {
+  if (/^pet_\d{4}[A-Za-z]+$/.test(input)) {
     result = "Valid Syntax";
   } else {
     result = "Invalid Syntax";
